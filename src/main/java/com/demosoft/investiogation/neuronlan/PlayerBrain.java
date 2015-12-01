b198c7a847d793770ca8d72807ec9687c1e349bf
Andrii Korkoshko <demosoft@bk.ru>
2015-12-01 17:52:12 +0200

    Xml parsing

---
+++ src/main/java/com/demosoft/investiogation/neuronlan/PlayerBrain.java
@@ -0,0 +1,7 @@
+package com.demosoft.investiogation.neuronlan;
+
+/**
+ * Created by Andrii_Korkoshko on 01.12.2015.
+ */
+public class PlayerBrain {
+}
--- src/main/java/com/demosoft/investiogation/neuronlan/xml/JAXB.java
+++ src/main/java/com/demosoft/investiogation/neuronlan/xml/JAXB.java
@@ -1,6 +1,8 @@
 package com.demosoft.investiogation.neuronlan.xml;
 
+import com.demosoft.investiogation.neuronlan.PlayerBrain;
 import com.demosoft.investiogation.neuronlan.xml.generated.network.Network;
+import com.demosoft.investiogation.neuronlan.xml.generated.network.Neuron;
 import com.demosoft.investiogation.neuronlan.xml.generated.network.ObjectFactory;
 
 import javax.xml.bind.JAXBContext;
@@ -9,6 +11,7 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.List;
 
 /**
  * Created by Andrii_Korkoshko on 01.12.2015.
@@ -34,6 +37,11 @@ public class JAXB {
         } catch (JAXBException e) {
             e.printStackTrace();
         }
+        return null;
+    }
+
+    public PlayerBrain compileStructure(Network network) {
+        List<Neuron> neurons = network.getNeuron();
         return null;
     }
 }
