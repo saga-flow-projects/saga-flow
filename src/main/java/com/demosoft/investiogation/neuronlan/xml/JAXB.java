333b4438719c649da8c2234843c5a959ea876757
Andrii Korkoshko <demosoft@bk.ru>
2015-12-01 16:41:00 +0200

    Xml parsing

--- src/main/java/com/demosoft/investiogation/neuronlan/xml/JAXB.java
+++ src/main/java/com/demosoft/investiogation/neuronlan/xml/JAXB.java
@@ -1,7 +1,39 @@
 package com.demosoft.investiogation.neuronlan.xml;
 
+import com.demosoft.investiogation.neuronlan.xml.generated.network.Network;
+import com.demosoft.investiogation.neuronlan.xml.generated.network.ObjectFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 /**
  * Created by Andrii_Korkoshko on 01.12.2015.
  */
 public class JAXB {
+    public static void main(String[] args) {
+        new JAXB().getStructure();
+
+    }
+
+    public Network getStructure() {
+        try {
+
+            Reader reader = new InputStreamReader(this.getClass().getClassLoader()
+                    .getResourceAsStream("simpleNetwork.xml"));
+            JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Network network = ((JAXBElement<Network>) jaxbUnmarshaller.unmarshal(reader)).getValue();
+            System.out.println(network);
+            return network;
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
